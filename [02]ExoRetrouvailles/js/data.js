2ee34ef8e7d63d878f7d035503f58f373f9a339a
johrdane93 <johrdane.ledra@lepoles.com>
2017-03-20 17:14:10 +0100

    ajout de exo retrouvaille

---
+++ [02]ExoRetrouvailles/js/data.js
@@ -0,0 +1,12 @@
+var data = {
+  'perso_0' : {
+    'name' : 'Chouwi',
+    'picture' : 'https://media.giphy.com/media/3o7TKrpHvJuS3KNrS8/giphy.gif',
+    'texte' : ['no money pas dgrand pere','ta des tune ','aller barre toi']
+  },
+  'perso_1' : {
+    'name' : 'Archibalde',
+    'picture' : 'https://media.giphy.com/media/l2JhpzAYL3rnNi3S0/giphy.gif',
+    'texte' : ['Grand père, c\'est moi!','J\'ai pas de tune g 1euro','Pff nimporte quoi!']
+  }
+}
